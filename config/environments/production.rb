1a356034b564430e3923b4d95b4c7fd8c3b3c490
Lisandro Gurman <lichun88@gmail.com>
2016-01-15 07:07:52 -0300

    se sacaron validaciones de openssl al mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
    :user_name            => 'prueba@thecraftbeertruck.com.ar',
    :password             => 'prueba2015',
    :authentication       => :plain,
-   :enable_starttls_auto => true
+   :enable_starttls_auto => true,
    :openssl_verify_mode  => 'none'
  }
 end
